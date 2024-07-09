ed14de4a01dbe2566d93238fa8e52f44500d1e65
Abhishek Sharma <117314498+Abhishek0348@users.noreply.github.com>
2024-07-09 11:58:14 +0530

    Bubble sort done

--- Sorting/sorting.cpp
+++ Sorting/sorting.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
-#include<bits/stdc++.h>
+#include <vector>
 
 using namespace std;
 
-bubblesort(int a[], int n){
+void printSortedArr(vector<int> arr, int n){
+    cout<<"Sorted Array : ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl; 
+}
 
+void bubblesort(vector<int>& arr, int n){
+    for (int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){ 
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printSortedArr(arr, n);
 }
 
 int main()
 {
-  vector<int> arr;
-  
+    vector<int> arr;
+    int n;
+    cout<<"Define Size of Arr : ";
+    cin >> n;
+
+    cout<<"Enter the nos in arr ";
+    for(int i = 0; i < n; i++){
+        int value;
+        cin >> value;
+        arr.push_back(value);
+    }
+
+    bubblesort(arr, n);
+
+    return 0;
 }
